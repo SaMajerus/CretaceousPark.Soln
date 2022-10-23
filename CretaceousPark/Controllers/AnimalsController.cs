98a4a129bc35c544d3278a6349449b062d2cda86
Samuel Majerus <ladolego@gmail.com>
2022-10-22 22:37:42 -0700

    Adds an 'AnimalsController.cs'.    [End of code-along work for Lsn 5 (see link)]. https://www.learnhowtoprogram.com/c-and-net/building-an-api/api-create-and-read

---
+++ CretaceousPark/Controllers/AnimalsController.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CretaceousPark.Models;
+
+namespace CretaceousPark.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class AnimalsController : ControllerBase
+  {
+    private readonly CretaceousParkContext _db;
+
+    public AnimalsController(CretaceousParkContext db)
+    {
+      _db = db;
+    }
+
+    // GET api/animals
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Animal>>> Get()
+    {
+      return await _db.Animals.ToListAsync();
+    }
+
+    // POST api/animals
+    [HttpPost]
+    public async Task<ActionResult<Animal>> Post(Animal animal)
+    {
+      _db.Animals.Add(animal);
+      await _db.SaveChangesAsync();
+
+      return CreatedAtAction("Post", new { id = animal.AnimalId }, animal);
+    }
+
+    // GET: api/Animals/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Animal>> GetAnimal(int id)
+    {
+      var animal = await _db.Animals.FindAsync(id);
+
+      if (animal == null)
+      {
+        return NotFound();
+      }
+
+      return animal;
+    }
+  }
+}
